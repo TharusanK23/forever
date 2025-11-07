e60d98ab324419bfe077c38c3e0d78a95754a1a8
Tharusan_K_23 <tharusan.study@gmail.com>
2025-11-07 15:54:07 +0530

    Updated

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,7 +4,7 @@ import productModel from '../models/productModel.js';
 // Add a new product
 const addProduct = async (req, res) => {
     try {
-        const { name, description, price, category, subCategory, sizes, bestSeller } = req.body;
+        const { name, description, price, category, subCategory, sizes, bestseller } = req.body;
 
         const image1 = req.files['image1'] && req.files['image1'][0];
         const image2 = req.files['image2'] && req.files['image2'][0];
@@ -26,12 +26,10 @@ const addProduct = async (req, res) => {
             category,
             subCategory,
             sizes: JSON.parse(sizes),
-            bestSeller: (bestSeller || bestSeller === 'true') ? true : false,
+            bestSeller: (bestseller || bestseller === 'true') ? true : false,
             image: imageUrls,
             date: Date.now()
         }
-
-        console.log(productData);
 
         const product = new productModel(productData);
         await product.save();
